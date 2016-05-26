1968c9963b7c69db92212f23a232b7ac37e2f09d
ascoelho <anthonystefancoelho@gmail.com>
2016-05-26 11:17:11 -0400

    first commit

---
+++ Receipts++/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Receipts++
+//
+//  Created by Anthony Coelho on 2016-05-26.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
